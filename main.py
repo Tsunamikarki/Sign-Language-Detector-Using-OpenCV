74b9ed2bef516ce76b19f38488bd0548afe21b84
Tsunamikarki <karkeesunamibns55@gmail.com>
2024-07-24 13:31:03 -0400

    Testing the webcam

---
+++ main.py
@@ -0,0 +1,14 @@
+import cv2 
+import numpy as np 
+
+video = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = video.read()
+    cv2.imshow('Video', frame)
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    
+video.release()
+cv2.destroyAllWindows()
